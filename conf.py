4de9b33791a892396c856ee47250fbdca405e032
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2017-03-10 05:27:10 +0100

    A propos de ...

--- conf.py
+++ conf.py
@@ -132,6 +132,7 @@ TRANSLATIONS_PATTERN = "{path}.{lang}.{ext}"
 
 NAVIGATION_LINKS = {
     DEFAULT_LANG: (
+        ("/a-propos-de-ce-blog/", "A propos..."),
         ("/archive.html", "Archive"),
         ("/categories/", "Tags"),
         ("/rss.xml", "RSS feed"),
